0184f47cdcfff2bd313143e5fa7fa516f879d48e
ferdinandjr-14 <fpintod2425@politecnics.barcelona>
2025-11-10 13:15:22 +0100

    Ruta y estructura de la Landing Page

---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ZERO</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <img src="" alt="zero logo">
+            <p>INICIO</p>
+            <p>HISTORIA</p>
+            <p>NIVELES</p>
+            <p>RANKING</p>
+            <p>EQUIPO</p>
+            <p>PERFIL</p>
+            <select name="language" id="language">
+                <option value="es" selected>ES</option>
+                <option value="ca">CA</option>
+                <option value="en">EN</option>
+            </select>
+        </nav>
+        <div>
+            <h1>¡AYUDA A ZERO A SALVAR EL PUEBLO DÍGITO!</h1>
+            <img src="" alt="zero character">
+            <button>JUGAR YA</button>
+        </div>
+    </header>
+    <main>
+        <section>
+            <h2>HISTORIA</h2>
+            <p>En Pueblo Dígito, la Energía Matemágica mantiene el equilibrio entre números, formas y medidas.
+                <br>
+                Pero un día, el Profesor Kaos, un inventor brillante pero despistado, libera por accidente a los
+                Errores: pequeñas criaturas que devoran números y desordenan las operaciones.
+                <br>
+                El caos se extiende por todo el reino, y solo Zero, un joven decidido que vive en las afueras, puede
+                restaurar la armonía.
+                Para lograrlo, deberá recuperar los cuatro Cristales Matemágicos, cada uno protegido en una región
+                distinta.
+            </p>
+            <div>
+                <img src="" alt="cristal 1">
+                <img src="" alt="cristal 2">
+                <img src="" alt="cristal 3">
+                <img src="" alt="cristal 4">
+            </div>
+            <p>
+                Cada cristal representa un desafío matemático que pondrá a prueba tu lógica, velocidad y habilidad.
+                <br>
+                ¿Ayudarás a Zero a dominar la Matemágia y salvar Pueblo Dígito del kaos?
+            </p>
+        </section>
+        <section>
+            <h2>¡DESBLOQUEA TODOS LOS NIVELES!</h2>
+            <img src="" alt="Pueblo digito">
+            <div>
+                <h3>EL BOSQUE DE LA SUMA</h3>
+                <img src="" alt="El Bosque de la Suma">
+            </div>
+            <div>
+                <h3>EL DESIERTO AVANZADO</h3>
+                <img src="" alt="El Desierto Avanzado">
+
+            </div>
+            <div>
+                <h3>LAS MONTAÑAS GEOMÉTRICAS</h3>
+                <img src="" alt="Las Montañas Geométricas">
+
+            </div>
+            <div>
+                <h3>LA CIUDAD DEL SABER</h3>
+                <img src="" alt="La Ciudad del Saber">
+            </div>
+        </section>
+        <section>
+            <h2>¡SUMA PUNTOS Y ENTRA EN EL RANKING!</h2>
+            <img src="" alt="Ejemplo ranking">
+        </section>
+        <section>
+            <h2>EQUIPO CREATIVO</h2>
+            <div class="card-team">
+                <img src="" alt="Ferdinand">
+                <p>Ferdinand Pinto</p>
+                <p>Rol</p>
+                <p>Responsabilidades</p>
+            </div>
+            <div class="card-team">
+                <img src="" alt="Eric">
+                <p>Eric Peralta</p>
+                <p>Rol</p>
+                <p>Responsabilidades</p>
+            </div>
+            <div class="card-team">
+                <img src="" alt="Brian">
+                <p>Brian</p>
+                <p>Rol</p>
+                <p>Responsabilidades</p>
+            </div>
+            <div class="card-team">
+                <img src="" alt="Max">
+                <p>Max Vidal</p>
+                <p>Rol</p>
+                <p>Responsabilidades</p>
+            </div>
+        </section>
+        <section>
+            <h2>CONTACTO</h2>
+            <form action="">
+                <label for="name">Nombre</label>
+                <input type="text" name="name" id="name">
+
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email">
+
+                <label for="message">Mensaje</label>
+                <textarea name="message" id="message"></textarea>
+
+                <button type="submit">SUBMIT</button>
+            </form>
+        </section>
+    </main>
+    <footer>
+        <div>
+            <img src="" alt="Zero logo">
+            <ul>
+                <li>INICIO</li>
+                <li>HISTORIA</li>
+                <li>NIVELES</li>
+                <li>RANKING</li>
+                <li>EQUIPO</li>
+            </ul>
+        </div>
+        <p>© 2025 ZERO / DAW2 - Todos los derechos reservados</p>
+    </footer>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/zero', function () {
+    return view('landingPage');
 });
