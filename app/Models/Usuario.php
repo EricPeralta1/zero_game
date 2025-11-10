ca9dfcbcde0c4eb4367ce162da8048fc6154ee78
bpiguavep2526-sketch <bpiguavep2526@politecnics.barcelona>
2025-11-10 17:47:38 +0100

    realacion usario rol

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\rol;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Usuario extends Model
 {
-    //
+     protected $table = 'usuarios';
+    protected $primaryKey = 'id_user';
+    public $timestamps = false;
+    protected $fillable=['nom_usuario','email','password' ,'id_rol'];
+
+    
+    
+    public function Rol(): BelongsTo
+    {
+        return $this->belongsTo(rol::class, 'id_rol');
+    }
+    
 }
--- app/Models/rol.php
+++ app/Models/rol.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use App\Models\Usuario;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class rol extends Model
 {
-    //
+     protected $table = 'roles';
+    protected $primaryKey = 'id_user';
+    public $timestamps = false;
+    protected $fillable=['nombre_rol'];
+
+    
+  /**
+   * Get all of thn Usuario for the rol
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\HasMany
+   */
+  public function Usuario(): HasMany
+  {
+      return $this->hasMany(Usuario::class, 'id_user');
+  }
+
 }
