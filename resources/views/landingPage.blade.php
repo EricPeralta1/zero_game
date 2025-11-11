bec545de431b702cae60645be6a5f2308c50f05a
raulrfc00 <rruizdelafuentec2223@politecnics.barcelona>
2025-11-11 18:02:02 +0100

    Implementación de mulitiidioma, modificación de la ruta de la Landing Page y se avanza el estilo

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ZERO</title>
-</head>
 <style>
     /* GLOBAL CONFIG */
     @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
@@ -19,7 +10,7 @@
 
     @font-face {
         font-family: 'Pixeloid';
-        src: url('../public/fonts/pixeloid.sans-bold.ttf') format('truetype');
+        src: url('../fonts/pixeloid.sans-bold.ttf') format('truetype');
         font-weight: normal;
         font-style: normal;
     }
@@ -40,12 +31,18 @@
     h1,
     h2,
     h3 {
-        font-family: 'Pixeloid';
-        color: var(--color-white)
+        font-family: 'Pixeloid', sans-serif;
+        color: var(--color-white);
+        text-align: center;
+    }
+
+    h2 {
+        margin: 40px 0;
     }
 
     h3 {
-        color: var(--color-orange)
+        color: var(--color-orange);
+        text-align: center;
     }
 
     p,
@@ -54,6 +51,7 @@
     ul {
         font-family: "VT323", monospace;
         color: var(--color-white);
+        font-size: 1.3rem;
     }
 
     a {
@@ -75,145 +73,295 @@
     }
 
     /* NAVBAR */
+    nav {
+        width: 100%;
+        display: flex;
+        align-content: center;
+        justify-content: center;
+        position: fixed;
+    }
+
+    nav>div {
+        width: 100%;
+        max-width: 1200px;
+        padding: 10px 20px;
+
+        display: flex;
+        align-content: center;
+        gap: 20px;
+    }
+
+    nav>div>img {
+        width: 120px;
+        height: auto;
+    }
+
+    nav>div a {
+        margin-top: auto;
+        margin-bottom: auto;
+        font-size: 1.8rem;
+    }
+
+    nav>div a:nth-child(2) {
+        margin-left: auto;
+    }
+
+    nav>div select {
+        margin-top: auto;
+        margin-bottom: auto;
+        height: max-content;
+        font-family: 'VT323';
+        font-size: 1.5rem;
+    }
 
     /* HERO */
     #inicio {
-        background-size: contain;
+        background-size: cover;
         background-repeat: no-repeat;
+        background-position: center;
+        height: 100%;
+        width: 100%;
+        width: auto;
+
+        display: flex;
+        justify-content: center;
+        align-content: center;
     }
 
-    #inicio>img {}
+    #inicio>div {
+        display: flex;
+        flex-direction: column;
+        max-width: 1200px;
+
+        justify-content: start;
+        align-content: center;
+        margin: auto 0 auto 0
+    }
+
+    #inicio>div>h1 {
+        font-size: 3rem;
+        text-align: start;
+    }
+
+    #inicio>div>button {
+        cursor: pointer;
+        max-width: 300px;
+        padding: 15px 60px;
+        font-size: 1.5rem;
+        border: 5px solid black;
+        font-family: 'Pixeloid', sans-serif;
+
+        transition: all 400ms;
+    }
+
+    #inicio>div>button:hover {
+        background: #c53b00;
+    }
+
+    #inicio img,
+    #inicio button {
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    #inicio img {
+        width: 200px;
+        height: auto;
+        z-index: 100;
+    }
+
+    /* MAIN */
+    main {
+        display: flex;
+        justify-content: center;
+    }
+
+    main>div {
+        max-width: 1200px;
+        width: 100%;
+    }
+
+    /* STORY SECTION */
+    #historia>div {
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        gap: 20px;
+        margin: 50px 0;
+    }
+
+    #historia>div>img {
+        height: 100px;
+        width: auto;
+    }
+
+    /* LEVELS SECTION*/
+    #niveles {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    #niveles>div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+    }
+
+    #niveles>div:nth-child(2)>img {
+        margin: 0 auto 40px auto;
+        width: 50%;
+        height: auto;
+    }
+
+    #niveles>div>img {
+        margin: 0 auto 0 auto;
+        width: 70%;
+        height: auto;
+    }
+
+    #niveles>div>h3 {
+        margin: 0;
+        font-size: 1.5rem;
+    }
 </style>
 
 <body>
     <header>
         <nav>
-            <img src="{{ asset('images/landingPage/zero_logo_nobg.png') }}" alt="zero logo">
-            <a href="#inicio">INICIO</a>
-            <a href="#historia">HISTORIA</a>
-            <a href="#niveles">NIVELES</a>
-            <a href="#ranking">RANKING</a>
-            <a href="#equipo">EQUIPO</a>
-            <a>PERFIL</a>
-            <select name="language" id="language">
-                <option value="es" selected>ES</option>
-                <option value="ca">CA</option>
-                <option value="en">EN</option>
-            </select>
+            <div>
+                <img src="{{ asset('images/landingPage/zero_logo_nobg.png') }}" alt="zero logo">
+
+                @foreach ($texts['navbar'] as $item)
+                    <a href="#">{{ $item }}</a>
+                @endforeach
+
+
+                <select name="language" id="select-language">
+                    @php
+                        $languages = ['es', 'ca', 'en'];
+                    @endphp
+                    @foreach ($languages as $language)
+                        <option value="{{ $language }}" @if ($language === $lang) selected @endif>
+                            {{ Str::upper($language) }}</option>
+                    @endforeach
+                </select>
+            </div>
         </nav>
         <div id="inicio" style="background-image:url({{ asset('images/landingPage/hero_img.png') }})">
-            <h1>¡AYUDA A ZERO A SALVAR EL PUEBLO DÍGITO!</h1>
-            <img src="{{ asset('images/landingPage/zero_fight.png') }}" alt="zero character">
-            <button>JUGAR YA</button>
+            <div>
+                <h1>{{ $texts['hero_title'] }}</h1>
+                <img src="{{ asset('images/landingPage/zero_fight.png') }}" alt="zero character">
+                <button>{{ $texts['hero_btn'] }}</button>
+            </div>
         </div>
     </header>
     <main>
-        <section id="historia">
-            <h2>HISTORIA</h2>
-            <p>En Pueblo Dígito, la Energía Matemágica mantiene el equilibrio entre números, formas y medidas.
-                <br>
-                Pero un día, el Profesor Kaos, un inventor brillante pero despistado, libera por accidente a los
-                Errores: pequeñas criaturas que devoran números y desordenan las operaciones.
-                <br>
-                El caos se extiende por todo el reino, y solo Zero, un joven decidido que vive en las afueras, puede
-                restaurar la armonía.
-                Para lograrlo, deberá recuperar los cuatro Cristales Matemágicos, cada uno protegido en una región
-                distinta.
-            </p>
-            <div>
-                <img src="{{ asset('images/gemas/gema_1.png') }}" alt="cristal 1" class="gema">
-                <img src="{{ asset('images/gemas/gema_2.png') }}" alt="cristal 2" class="gema">
-                <img src="{{ asset('images/gemas/gema_3.png') }}" alt="cristal 3" class="gema">
-                <img src="{{ asset('images/gemas/gema_4.png') }}" alt="cristal 4" class="gema">
-            </div>
-            <p>
-                Cada cristal representa un desafío matemático que pondrá a prueba tu lógica, velocidad y habilidad.
-                <br>
-                ¿Ayudarás a Zero a dominar la Matemágia y salvar Pueblo Dígito del kaos?
-            </p>
-        </section>
-        <section id="niveles">
-            <h2>¡DESBLOQUEA TODOS LOS NIVELES!</h2>
-            <img src="{{ asset('images/landingPage/landing_lvl1.png') }}" alt="Pueblo digito">
-            <div>
-                <h3>EL BOSQUE DE LA SUMA</h3>
-                <img src="{{ asset('images/landingPage/landing_lvl_blocked.png') }}" alt="El Bosque de la Suma">
-            </div>
-            <div>
-                <h3>EL DESIERTO AVANZADO</h3>
-                <img src="{{ asset('images/landingPage/landing_lvl_blocked.png') }}" alt="El Desierto Avanzado">
+        <div>
 
-            </div>
-            <div>
-                <h3>LAS MONTAÑAS GEOMÉTRICAS</h3>
-                <img src="{{ asset('images/landingPage/landing_lvl_blocked.png') }}" alt="Las Montañas Geométricas">
+            <section id="historia">
+                <h2>{{ $texts['story_section']['title'] }}</h2>
+                {!! $texts['story_section']['text'][0] !!}
 
-            </div>
-            <div>
-                <h3>LA CIUDAD DEL SABER</h3>
-                <img src="{{ asset('images/landingPage/landing_lvl_blocked.png') }}" alt="La Ciudad del Saber">
-            </div>
-        </section>
-        <section id="ranking">
-            <h2>¡SUMA PUNTOS Y ENTRA EN EL RANKING!</h2>
-            <img src="" alt="Ejemplo ranking">
-        </section>
-        <section id="equipo">
-            <h2>EQUIPO CREATIVO</h2>
-            <div class="card-team">
-                <img src="" alt="Ferdinand">
-                <p>Ferdinand Pinto</p>
-                <p>Rol</p>
-                <p>Responsabilidades</p>
-            </div>
-            <div class="card-team">
-                <img src="" alt="Eric">
-                <p>Eric Peralta</p>
-                <p>Rol</p>
-                <p>Responsabilidades</p>
-            </div>
-            <div class="card-team">
-                <img src="" alt="Brian">
-                <p>Brian</p>
-                <p>Rol</p>
-                <p>Responsabilidades</p>
-            </div>
-            <div class="card-team">
-                <img src="" alt="Max">
-                <p>Max Vidal</p>
-                <p>Rol</p>
-                <p>Responsabilidades</p>
-            </div>
-        </section>
-        <section id="contacto">
-            <h2>CONTACTO</h2>
-            <form action="">
-                <label for="name">Nombre</label>
-                <input type="text" name="name" id="name">
+                <div>
+                    <img src="{{ asset('images/gemas/gema_1.png') }}" alt="cristal 1" class="gema">
+                    <img src="{{ asset('images/gemas/gema_2.png') }}" alt="cristal 2" class="gema">
+                    <img src="{{ asset('images/gemas/gema_3.png') }}" alt="cristal 3" class="gema">
+                    <img src="{{ asset('images/gemas/gema_4.png') }}" alt="cristal 4" class="gema">
+                </div>
 
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email">
+                {!! $texts['story_section']['text'][1] !!}
+            </section>
+            <section id="niveles">
+                <h2>{{ $texts['levels_section']['title'] }}</h2>
+                <div>
+                    <h3>{{ $texts['levels_section']['lvl_titles'][0] }}</h3>
+                    <img src="{{ asset('images/landingPage/landing_lvl1.png') }}" alt="Pueblo digito">
+                </div>
+                <div>
+                    <h3>{{ $texts['levels_section']['lvl_titles'][1] }}</h3>
+                    <img src="{{ asset('images/landingPage/landing_lvl_blocked.png') }}" alt="El Desierto Avanzado">
 
-                <label for="message">Mensaje</label>
-                <textarea name="message" id="message"></textarea>
+                </div>
+                <div>
+                    <h3>{{ $texts['levels_section']['lvl_titles'][2] }}</h3>
+                    <img src="{{ asset('images/landingPage/landing_lvl_blocked.png') }}"
+                        alt="Las Montañas Geométricas">
 
-                <button type="submit">SUBMIT</button>
-            </form>
-        </section>
+                </div>
+                <div>
+                    <h3>{{ $texts['levels_section']['lvl_titles'][3] }}</h3>
+                    <img src="{{ asset('images/landingPage/landing_lvl_blocked.png') }}" alt="La Ciudad del Saber">
+                </div>
+            </section>
+            <section id="ranking">
+                <h2>{{ $texts['ranking_section']['title'] }}</h2>
+                <img src="" alt="Ejemplo ranking">
+            </section>
+            <section id="equipo">
+                <h2>{{ $texts['team_section']['title'] }}</h2>
+                <div class="card-team">
+                    <img src="" alt="Ferdinand">
+                    <p>Ferdinand Pinto</p>
+                    <p>{{ $texts['team_section']['team']['max']['role'] }}</p>
+                    <p>{{ $texts['team_section']['team']['tasks_title'] }}</p>
+                </div>
+                <div class="card-team">
+                    <img src="" alt="Eric">
+                    <p>Eric Peralta</p>
+                    <p>{{ $texts['team_section']['team']['max']['role'] }}</p>
+                    <p>{{ $texts['team_section']['team']['tasks_title'] }}</p>
+                </div>
+                <div class="card-team">
+                    <img src="" alt="Brian">
+                    <p>Brian</p>
+                    <p>{{ $texts['team_section']['team']['max']['role'] }}</p>
+                    <p>{{ $texts['team_section']['team']['tasks_title'] }}</p>
+                </div>
+                <div class="card-team">
+                    <img src="" alt="Max">
+                    <p>Max Vidal</p>
+                    <p>{{ $texts['team_section']['team']['max']['role'] }}</p>
+                    <p>{{ $texts['team_section']['team']['tasks_title'] }}</p>
+                </div>
+            </section>
+            <section id="contacto">
+                <h2>{{ $texts['contact_section']['title'] }}</h2>
+                <form action="">
+                    <label for="name">{{ $texts['contact_section']['form_labels'][0] }}</label>
+                    <input type="text" name="name" id="name">
+
+                    <label for="email">{{ $texts['contact_section']['form_labels'][1] }}</label>
+                    <input type="email" name="email" id="email">
+
+                    <label for="message">{{ $texts['contact_section']['form_labels'][2] }}</label>
+                    <textarea name="message" id="message"></textarea>
+
+                    <button type="submit">{{ $texts['contact_section']['form_labels'][3] }}</button>
+                </form>
+            </section>
+        </div>
     </main>
     <footer>
         <div>
-            <img src="{{ asset('images/landingPage/zero_logo_og.png') }}" alt="Zero logo">
-            <ul>
-                <li><a href="#inicio">INICIO</a></li>
-                <li><a href="#historia">HISTORIA</a></li>
-                <li><a href="#niveles">NIVELES</a></li>
-                <li><a href="#ranking">RANKING</a></li>
-                <li><a href="#equipo">EQUIPO</a></li>
-            </ul>
+            <div>
+                <img src="{{ asset('images/landingPage/zero_logo_og.png') }}" alt="Zero logo">
+                <ul>
+                    @foreach ($texts['navbar'] as $item)
+                        <li><a href="#">{{ $item }}</a></li>
+                    @endforeach
+                </ul>
+            </div>
+            <p>{{ $texts['footer_section']['copyright'] }}</p>
         </div>
-        <p>© 2025 ZERO / DAW2 - Todos los derechos reservados</p>
     </footer>
 </body>
 
 </html>
+<script>
+    const selectLanguage = document.getElementById("select-language");
+    selectLanguage.addEventListener("change", function() {
+        window.location.href = `./${selectLanguage.value}`
+    })
+</script>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/zero', function () {
-    return view('landingPage');
+Route::get("/zero/{lang}", function ($lang) {
+    $lang = in_array($lang, ["en","es","ca"]) ? $lang : "en";
+
+    $jsonPath = "../resources/data/landing.json";
+    $jsonContent = file_get_contents($jsonPath);
+    $translations = json_decode($jsonContent, true);
+
+    $texts = $translations[$lang] ?? $translations["en"];
+
+    return view("landingPage", compact("texts", "lang"));
 });
