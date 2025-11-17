f894d4d6c3f374ca0f3721e16c24d0720bd68ea1
Eric Peralta <eperaltas2324@politecnics.barcelona>
2025-11-17 16:29:44 +0100

    Imagenes, conexión a bd para cargar puntuaciones y seleccionar puntuaciones por juego, cambio de pestaña.

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JuegoController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\PuntuacionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/levels', JuegoController::class);
-
-
+/*LOGIN Y REGISTRO*/
 route:: get ('/Login',[LoginController::class, 'showLogin'])->name('login');
 route::post ('/Login',[LoginController::class, 'Login'])->name('login.submit');
 
-
-
 route::post ('/Registro',[LoginController::class, 'store'])->name('usuario.store');
 route:: get ('/Registro',[UsuarioController::class, 'index'])->name('templates.Registro');
+
+/*RUTAS DE NIVELES Y CLASSIFICACION*/
+Route::get('/levels', [JuegoController::class, 'index'])->name('levels.index');
+Route::get('/leaderboard', [PuntuacionController::class, 'index'])->name('score.index');
+
+
+
