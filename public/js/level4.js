91335a9c53aafa4b5af2b993a64e6527012ed309
ferdinandjr-14 <fpintod2425@politecnics.barcelona>
2025-11-26 16:09:59 +0100

    cambios

--- public/js/level4.js
+++ public/js/level4.js
@@ -1,10 +1,21 @@
+// HP
 const hp = 5;
+let currentHP = hp;
 const hpContainer = document.getElementById("hp-container");
 const hpImg = `<img src="../Images/heart.png" alt="HP">`;
 
 function checkHP() {
-    for (let i = 0; i < hp; i++) {
+    for (let i = 0; i < currentHP; i++) {
         hpContainer.insertAdjacentHTML("beforeend", hpImg);
     }
 }
 checkHP();
+
+// GAME CONFIG
+const gameConfig = {
+    errorSpeed: 2,
+    spawnInterval: 3000,
+    errorSize: 80,
+};
+
+// GAME 
